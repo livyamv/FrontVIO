bf97bab04fe60d94d2234737c274c94519f4a59e
livyamv <liviareis2702@gmail.com>
2025-08-18 14:13:38 -0300

    18-08

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,42 +1,64 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
-  headers: {
-    accept: "application/json",
-  },
+    baseURL:"http://localhost:5000/api/v1/",
+    headers:{
+        'accept':'application/json'
+    }
 });
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    console.log(token);
-    if (token) {
-      config.headers.Authorization = `${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+    (config) =>{
+        const token = localStorage.getItem("token");
+        console.log(token)
+        if(token){
+            config.headers.Authorization = `${token}`;
+        }
+        return config;
+    },(error) => Promise.reject(error)
 );
 
-const sheets = {
-  getUsers: () => api.get("user"),
-  postLogin: (user) => api.post("login/", user),
-  deleteUser: (id) => api.delete("user/" + id),
-  getEvento: () => api.get("evento"),
-  deleteEvento: (id) => api.delete("evento/" + id),
-  createEvento: (form, imagem) => {
-    const data = new FormData();
-    for (let key in form) data.append(key, form[key]);
-    if(imagem) data.append("imagem",imagem);
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if(error.response){
+            if(error.response.status === 401 && error.response.data.auth === false){
+                localStorage.setItem("refresh_token",true)
+            localStorage.removeItem("token");
+            localStorage.removeItem("authenticated");
+            window.location.href = "/";
+            }
+             else if(error.response.status === 403 && error.response.data.auth === false){
+                localStorage.setItem("refresh_token",true)
+            localStorage.removeItem("token");
+            localStorage.removeItem("authenticated");
+            window.location.href = "/";
+        }
+    }
+    return Promise.reject(error);
+    }
+)
 
-    return api.post("/evento",data,{
-      headers:{
-        "Content-Type":"multpart/form-data",
-        Accept:"application/json",
-      }
-    })
-  },
-};
+const sheets = {
+    getUsers:()=>api.get("user"),
+    postLogin:(user) => api.post("login/", user),
+    deleteUser:(id) => api.delete("user/"+id),
+    getEvento:()=> api.get("evento"),
+    deleteEvento:(id) => api.delete("evento/"+id),
+    createIngresso: (ingresso) => api.post("/ingresso", ingresso),
+
+    createEvento: (form,imagem) => {
+        const data = new FormData();
+        for (let key in form) data.append(key, form[key]);
+        if(imagem) data.append("imagem", imagem);
+
+        return api.post ("/evento",data,{
+        headers:{
+            "Content-Type":"multpart/form-data",
+            Accept:"application/json"
+        }
+        })
+    }
+}
 
 export default sheets;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,9 +6,10 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { Link , useNavigate} from "react-router-dom";
-import { useState } from "react";
-import api from "../axios/axios"
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Snackbar } from "@mui/material";
+import { useState, useEffect } from "react";
+import api from "../axios/axios";
 
 
 function Login() {
@@ -16,6 +17,7 @@ function Login() {
     email: "",
     password: "",
   });
+  
   const navigate = useNavigate();
   const onChange = (event) => {
     const { name, value } = event.target;
@@ -27,23 +29,61 @@ function Login() {
     login();
   };
 
-  async function login(){
-    await api.postLogin(user).then(
-      (response)=>{
-        alert(response.data.message)
-        localStorage.setItem('authenticated',true)
+  async function login() {
+    try {
+      const response = await api.postLogin(user)
+      showAlert("sucess", response.data.message);
+        localStorage.setItem('authenticated', true)
         localStorage.setItem("token", response.data.token);
         navigate("users/")
-      },
-    (error)=>{
-      console.log(error)
-      alert(error.response.data.error)
+    }catch (error) {
+      showAlert("error",error.response.data.error)
     }
-    )
   }
+
+  useEffect(()=>{
+    const refreshToken = localStorage.getItem("refresh_token");
+    if(refreshToken){
+      showAlert("warning", "Sua sessão expirou. Faça login novamente")
+    }
+  },[])
+
+  const [alert, setAlert] = useState({
+    // visibilidade (false = oculto; true = visível)
+    open: false,
+
+    // nível do alerta (sucess, error, warning, etc)
+    severity: "",
+
+    // mensagem que será exibida
+    message: "",
+  });
+    const showAlert = (severity, message) => {
+    setAlert({ open: true, severity, message });
+    localStorage.removeItem("refresh_token");
+  };
+
+  // fechar o alerta
+  const handleCloseAlert = () => {
+    setAlert({ ...alert, open: false });
+  };
 
   return (
     <Container component="main" maxWidth="xl">
+         <Snackbar
+        open={alert.open}
+        autoHideDuration={3000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert
+          onClose={handleCloseAlert}
+          severity={alert.severity}
+          sx={{ width: "100%" }}
+        >
+          {alert.message}
+        </Alert>
+      </Snackbar>
       <Box
         sx={{
           marginTop: 8,
@@ -55,7 +95,7 @@ function Login() {
         <Avatar
           sx={{
             margin: 1,
-            backgroundColor: "#FFCBE8",
+            backgroundColor: "#FFC2D1",
           }}
         >
           <LockOutlinedIcon />
@@ -99,7 +139,7 @@ function Login() {
             sx={{
               mt: 3,
               mb: 2,
-              backgroundColor: "#FC8BC3",
+              backgroundColor: "#FFB3C6",
             }}
           >
             Entrar
@@ -110,9 +150,9 @@ function Login() {
             sx={{
               mt: 3,
               mb: 2,
-              backgroundColor: "#FC8BC3",
+              backgroundColor: "#FFB3C6",
             }}>
-            <Link to="/CreateEvent">Cadastro</Link>
+            <Link to="/cadastro">Cadastro</Link>
           </Button>
         </Box>
         
